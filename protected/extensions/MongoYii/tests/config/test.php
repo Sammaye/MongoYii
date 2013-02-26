532af648f0296190a346b44284054c21fd56a168
Sammaye <sam.millman@googlemail.com>
2013-02-26 09:59:56 +0000

    Forgot to commit the test configuration

---
+++ protected/extensions/MongoYii/tests/config/test.php
@@ -0,0 +1,13 @@
+<?php
+return CMap::mergeArray(
+    require('../../../config/main.php'),
+    array(
+        'components'=>array(
+			'mongodb' => array(
+				'class' => 'EMongoClient',
+				'server' => 'mongodb://localhost:27017',
+				'db' => 'super_test'
+			),
+        ),
+    )
+);
