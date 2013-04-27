7cfba2ae784f0c4f1668234f7f9bcd47bd4de805
Sammaye <sam.millman@googlemail.com>
2013-04-27 16:41:41 +0100

    Added fix for exception handling

--- EMongoClient.php
+++ EMongoClient.php
@@ -336,4 +336,24 @@ class EMongoClient extends CApplicationComponent{
 	}
 }
 
-class EMongoException extends CException {}
+class EMongoException extends CException
+{
+	/**
+	 * @var mixed the error info provided by a PDO exception. This is the same as returned
+	 * by {@link http://www.php.net/manual/en/pdo.errorinfo.php PDO::errorInfo}.
+	 * @since 1.1.4
+	 */
+	public $errorInfo;
+
+	/**
+	 * Constructor.
+	 * @param string $message PDO error message
+	 * @param integer $code PDO error code
+	 * @param mixed $errorInfo PDO error info
+	 */
+	public function __construct($message,$code=0,$errorInfo=null)
+	{
+		$this->errorInfo=$errorInfo;
+		parent::__construct($message,$code);
+	}
+}
