9822d2a31e32cd60b53be8fa8a1ad89182603b9e
Sammaye <sam.millman@googlemail.com>
2014-04-28 12:13:50 +0100

    Fixed: https://github.com/Sammaye/MongoYii/issues/206

--- EMongoDocument.php
+++ EMongoDocument.php
@@ -758,6 +758,16 @@ class EMongoDocument extends EMongoModel{
 	public function findAll($criteria = array(), $fields = array()){
 		return $this->find($criteria, $fields);
 	}
+	
+	/**
+	 * Alias of find
+	 * @param array $criteria
+	 * @param array|string[] $fields
+	 * @return EMongoCursor|EMongoDocument[]
+	 */
+	public function findAllByAttributes($criteria = array(), $fields = array()){
+		return $this->find($criteria, $fields);
+	}
 
 	/**
 	 * Finds all records based on $pk
